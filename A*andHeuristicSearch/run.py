e36a54dfa966545b824425e326c1f4b026422f15
mckellroberts <mckellkroberts@gmail.com>
2025-09-09 20:43:21 -0600

    Added a printResults function to format results better

--- A*andHeuristicSearch/run.py
+++ A*andHeuristicSearch/run.py
@@ -1,15 +1,24 @@
 from puzzle import EightPuzzle
 from searchCore import AStar
 
+def printResult(result):
+    print(f"Solution: {', '.join(result['solution'])}")
+    print(f"Cost: {result['cost']}")
+    print(f"Depth: {result['depth']}")
+    print(f"Nodes Expanded: {result['nodesExpanded']}")
+    print(f"Nodes Generated: {result['nodesGenerated']}")
+    print(f"Max Frontier Size: {result['maxFrontierSize']}")
+    print()  # blank line for readability
+
 if __name__ == "__main__":
     # Example unsolved state
-    initial_state = (4, 1, 3,
+    initialState = ( 4, 1, 3,
                      2, 6, 8,
                      7, 5, 0)
 
-    problem = EightPuzzle(initial_state)
+    problem = EightPuzzle(initialState)
 
     for h in ["h0", "h1", "h2"]:
         print(f"\nRunning A* with {h}...")
         result = AStar(problem, heuristicVariant=h)
-        print(result)
+        printResult(result)
